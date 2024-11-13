e00d08d9d888ca351fe53be5c7c2b2637b9155e5
ora7445 <luanqiu@gmail.com>
2024-11-13 13:44:34 -0600

    Update streamit_app.py
    change to actual chosen fruit

--- streamit_app.py
+++ streamit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie !:cup_with_straw:")
@@ -31,14 +29,14 @@ ingredients_list = st.multiselect(
      max_selections=5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json())
-
 if ingredients_list:
     ingredients_string =''
-    for fruit_choosen in ingredients_list:
-        ingredients_string += fruit_choosen + ' '  
-        
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '  
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
