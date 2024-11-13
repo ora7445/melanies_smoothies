85e08b68159367a092dfa4fa1d643e49c91f8258
ora7445 <luanqiu@gmail.com>
2024-11-13 12:09:55 -0600

    Update streamit_app.py
    add connection

--- streamit_app.py
+++ streamit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,7 +16,9 @@ st.write('The name on your smoothi will be:', name_on_order)
 #
 # st.write('Your favorite fruit is:', Option)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cns.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
