1febc1fb3a482f647d1354716df54c9e1a014f15
ora7445 <luanqiu@gmail.com>
2024-11-13 13:11:27 -0600

    Update streamit_app.py
    add missing )

--- streamit_app.py
+++ streamit_app.py
@@ -26,7 +26,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response).json()
+st.text(smoothiefroot_response).json())
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
